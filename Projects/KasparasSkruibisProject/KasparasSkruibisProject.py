70e0d2d5a9ef12a7f999dcc963b627686cd25322
Kasparas <kasparasskruibis@gmail.com>
2019-03-26 21:27:10 +0000

    Update

--- Projects/KasparasSkruibisProject/KasparasSkruibisProject.py
+++ Projects/KasparasSkruibisProject/KasparasSkruibisProject.py
@@ -2,35 +2,34 @@
 
 
 def load_data():
-    openData = open('data.txt', 'r')
-    idList = []
-    fnameList = []
-    lnameList = []
-    emailList = []
-    salaryList = []
-    while True:
-        with openData as f:
-            line = f.read()
-
+    dataLists = [[], [], [], [], []]
+    i = 0
+    with open('data.txt') as f:
+        for i, line in enumerate(f):
+            dataLists[i % 5].append(line.strip())
+    return dataLists
 
 
 def show_menu():
     print(30 * "-", "MENU", 30 * "-")
-    print("1. Menu Option 1")
+    print("1. Show all employees")
     print("2. Menu Option 2")
     print("3. Menu Option 3")
     print("4. Menu Option 4")
     print("5. Exit")
     print(67 * "-")
     loop = True
-
+    dataLists = load_data()
     while loop:
         try:
             choice = int(input("Enter your choice [1-5]: "))
         except:
             choice = 'error'
         if choice == 1:
-            print("Menu 1 has been selected")
+            print("Showing employees")
+            for i in range(len(dataLists[1])):
+                print("ID:", dataLists[0][i], "|", "Name:", dataLists[1][i], dataLists[2][i], "|", "Email:",
+                      dataLists[3][i], "|", "Salary:", dataLists[4][i])
             loop = False
         elif choice == 2:
             print("Menu 2 has been selected")
@@ -53,7 +52,6 @@ def save_data():
 
 
 def main():
-    load_data()
     show_menu()
     save_data()
 
