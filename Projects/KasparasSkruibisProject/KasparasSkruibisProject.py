3a8e8a08c252704957c212225ebd58993a17c1d6
Kasparas Skruibis <kasparasskruibis@gmail.com>
2019-03-26 14:53:47 +0000

    Test

--- Projects/KasparasSkruibisProject/KasparasSkruibisProject.py
+++ Projects/KasparasSkruibisProject/KasparasSkruibisProject.py
@@ -2,11 +2,50 @@
 
 
 def load_data():
-    print('Test1')
+    openData = open('data.txt', 'r')
+    idList = []
+    fnameList = []
+    lnameList = []
+    emailList = []
+    salaryList = []
+    while True:
+        with openData as f:
+            line = f.read()
+
 
 
 def show_menu():
-    print('Test2')
+    print(30 * "-", "MENU", 30 * "-")
+    print("1. Menu Option 1")
+    print("2. Menu Option 2")
+    print("3. Menu Option 3")
+    print("4. Menu Option 4")
+    print("5. Exit")
+    print(67 * "-")
+    loop = True
+
+    while loop:
+        try:
+            choice = int(input("Enter your choice [1-5]: "))
+        except:
+            choice = 'error'
+        if choice == 1:
+            print("Menu 1 has been selected")
+            loop = False
+        elif choice == 2:
+            print("Menu 2 has been selected")
+            loop = False
+        elif choice == 3:
+            print("Menu 3 has been selected")
+            loop = False
+        elif choice == 4:
+            print("Menu 4 has been selected")
+            loop = False
+        elif choice == 5:
+            print("Menu 5 has been selected")
+            loop = False
+        else:
+            print("Wrong option selection. Enter any key to try again..")
 
 
 def save_data():
@@ -17,5 +56,6 @@ def main():
     load_data()
     show_menu()
     save_data()
+
 
 main()
